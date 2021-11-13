42b9f1034e3feda0aeffc53f0d3738b603ccf3fe
ariisov <argon328@gmail.com>
2021-11-13 14:48:18 +0300

    Initial Commit

---
+++ PrayInTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrayInTime
+//
+//  Created by Булат Хатмуллин on 13.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
